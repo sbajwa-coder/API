d92b2903d3fb46e2e8ae5f1c25dc0a25548fd33e
Ubuntu <ubuntu@ip-172-26-2-14.ca-central-1.compute.internal>
2019-02-20 18:57:59 +0000

    installed some extra useful packages for REST

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('SLS - testing GUI3!\n');
+  res.end('SLS - testing GUI4!\n');
 });
 
 server.listen(port, hostname, () => {
